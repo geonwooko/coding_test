551094b8b6956efc3cf73382c10aac71cb5f5136
Geonwoo Ko <67784610+geonwooko@users.noreply.github.com>
2024-11-13 14:01:29 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,23 @@
+-- 코드를 입력하세요
+
+WITH Q AS 
+(
+    SELECT 
+        WRITER_ID
+    FROM
+        USED_GOODS_BOARD
+    GROUP BY
+        WRITER_ID
+    HAVING
+        COUNT(*) >= 3
+)
+    
+SELECT 
+    USER_ID, 
+    NICKNAME, 
+    CONCAT(CITY, ' ', STREET_ADDRESS1, ' ', STREET_ADDRESS2) 전체주소,
+    CONCAT(SUBSTR(U.TLNO, 1, 3), '-', SUBSTR(U.TLNO, 4, 4), '-', SUBSTR(U.TLNO, 8, 4)) 전화번호
+FROM
+    USED_GOODS_USER U JOIN Q ON U.USER_ID = Q.WRITER_ID
+ORDER BY USER_ID DESC
+
