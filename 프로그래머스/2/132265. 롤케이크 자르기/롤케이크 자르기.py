8ccf4ae074504dd68d7a5e821271e2aaa10d4806
Geonwoo Ko <67784610+geonwooko@users.noreply.github.com>
2025-08-27 13:25:23 +0900

    [level 2] Title: 롤케이크 자르기, Time: 643.54 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,14 @@
-from collections import Counter
-def solution(topping):
-    hyung, dongsang = dict(), Counter(topping)
-    answer = 0
-    for i in range(len(topping)):
-        cur_top = topping[i]
-        hyung[cur_top] = hyung.get(cur_top, 0) + 1
-        if dongsang[cur_top] == 1:
-            del dongsang[cur_top]
-        else:
-            dongsang[cur_top] -= 1
-        if len(hyung) == len(dongsang):
-            answer += 1
-    return answer
-    
+from collections import deque, Counter
+def solution(s):
+    left_counter = dict(Counter(s))
+    dq = deque(s)
+    res = 0
+    right_set = set()
+    for i in range(len(s)):
+        el = dq.pop()
+        right_set.add(el)
+        left_counter[el] -= 1
+        if left_counter[el] == 0:
+            del left_counter[el]
+        res += int(len(left_counter) == len(right_set))
+    return res
