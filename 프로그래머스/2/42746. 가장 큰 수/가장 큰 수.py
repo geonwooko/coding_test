45f25f0d3e3e165d0b723793a0df9085c96fb850
Geonwoo Ko <67784610+geonwooko@users.noreply.github.com>
2025-08-30 18:01:12 +0900

    [level 2] Title: 가장 큰 수, Time: 2051.66 ms, Memory: 23.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,17 @@
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    if int(t1) > int(t2):
+        return 1
+    elif int(t1) < int(t2):
+        return -1
+    else:
+        return 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
